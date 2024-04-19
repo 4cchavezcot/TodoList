bc70b99a0ac1d46403035ce506e24114b50e909a
cchavezcot <cchavezcot@unprg.edu.pe>
2024-04-19 02:45:44 -0500

    add1

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,6 +71,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: theme => ({
+        'imagen-fondo': "url('./assets/fondo1.jpg')",
+      })
     },
   },
   plugins: [require("tailwindcss-animate")],
